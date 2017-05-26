77be3e8018121c52f0f592fab2d59d2cdf2f91f4
Alohaoe <giasu711@yahoo.co.jp>
2017-05-26 09:58:39 +0900

    bootstrap and omikuji

---
+++ js/omikuji/js/main.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    var btn = document.getElementById('btn');
+
+    btn.addEventListener('click', function() {
+        var n = Math.floor(Math.random() * 3);
+        this.textContent = n;
+    });
+    btn.addEventListener('mousedown', function() {
+        this.className = 'pushed';
+    });
+    btn.addEventListener('mouseup', function() {
+        this.className = '';
+    });
+})();
