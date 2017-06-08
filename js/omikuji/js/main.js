675410da25e406b6fb31e3bad6e01c19c0f4da21
Alohaoe <giasu711@yahoo.co.jp>
2017-06-08 09:55:53 +0900

    Finish omikuji, start 5seconds

--- js/omikuji/js/main.js
+++ js/omikuji/js/main.js
@@ -4,8 +4,18 @@
     var btn = document.getElementById('btn');
 
     btn.addEventListener('click', function() {
-        var n = Math.floor(Math.random() * 3);
-        this.textContent = n;
+        // var results = ['大吉', '中吉', '凶', '末吉'];
+        // var results = ['大吉', '大吉', '大吉', '大吉', '凶'];
+        // var n = Math.floor(Math.random() * results.length);
+        // this.textContent = results[n];
+        var n = Math.random();
+        if (n < 0.05) {
+            this.textContent = '大吉'; // 5%
+        } else if (n < 0.2) {
+            this.textContent = '中吉'; // 15%
+        } else {
+            this.textContent = '凶';
+        }
     });
     btn.addEventListener('mousedown', function() {
         this.className = 'pushed';
